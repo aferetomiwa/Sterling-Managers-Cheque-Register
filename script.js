5fb37f68e29c2cf8bfb88212c2675c1332b28f14
aferetomiwa <aferetomiwa@gmail.com>
2022-06-28 19:14:33 +0100

    merged individual projects

---
+++ script.js
@@ -0,0 +1,9 @@
+const main = document.getElementById("main");
+
+
+main.addEventListener("click", function no() {
+    style.display = "block";
+})
+main.addEventListener("click", function no() {
+    style.display = "none";
+})
